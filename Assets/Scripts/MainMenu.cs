83794113b1793ec9d7a2be3fa625d046fb358211
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-09-07 11:29:56 -0500

    Added Quit button functions

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,19 +8,25 @@ using System.Collections.Generic;
 public class MainMenu : MonoBehaviour
 {
     public Button playButton;
+    public Button quitButton;
 
      ///<summary>Initializing game from menu screen</summary>
     void Start()
     {
         playButton.onClick.AddListener(PlayMaze);
+        quitButton.onClick.AddListener(QuitMaze);
     }
 
     void Update()
     {
-
+        
     }
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze");
+    }
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
     }
 }
