31ee1116faf922d328ae99d772be5d5009671ec2
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-09-07 19:01:37 -0500

    Adding colorblind mode

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,6 +9,9 @@ public class MainMenu : MonoBehaviour
 {
     public Button playButton;
     public Button quitButton;
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
     //public Button optionsButton;
     //public Button backButton;
 
@@ -25,28 +28,39 @@ public class MainMenu : MonoBehaviour
         //backButton.onClick.AddListener(OpenMainMenu);
     }
 
-    void Update()
-    {
+    // void Update()
+    // {
         
-    }
+    // }
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene("maze");
     }
     public void QuitMaze()
     {
         Debug.Log("Quit Game");
+        Application.Quit();
     }
-        public void OpenOptionsMenu()
-    {
-        mainMenu.SetActive(false);
-        optionsMenu.SetActive(true);
-    }
-    public void OpenMainMenu()
-    {
-        optionsMenu.SetActive(false);
-        mainMenu.SetActive(true);
-    }
+    //   public void OpenOptionsMenu()
+    // {
+    //     mainMenu.SetActive(false);
+    //     optionsMenu.SetActive(true);
+    // }
+    //     public void OpenMainMenu()
+    // {
+    //     optionsMenu.SetActive(false);
+    //     mainMenu.SetActive(true);
+    // }
 
     
 }
